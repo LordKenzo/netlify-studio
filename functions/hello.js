b2d59dfd65bdc929197268465879123c52d06b2d
lordkenzo <lordkenzo@outlook.com>
2020-05-23 23:58:56 +0200

    fix

--- functions/hello.js
+++ functions/hello.js
@@ -1,22 +1,11 @@
-const figlet = require('figlet');
+import { camelCase } from 'camel-case';
 
 exports.handler = async (event) => {
   const subject = event.queryStringParameters.name || 'World';
-  figlet(`Hello ${subject}!!`, function (err, data) {
-    if (err) {
-      console.error('Something went wrong...');
-      console.dir(err);
-      return {
-        statusCode: 500,
-        body: {
-          error: 'Something went wrong...',
-        },
-      };
-    }
-    console.log(data);
-    return {
-      statusCode: 200,
-      body: data,
-    };
-  });
+  camelCase(`Hello.${subject}`); //=> "helloWorld"
+  console.log(data);
+  return {
+    statusCode: 200,
+    body: data,
+  };
 };
