d1e6c27d1ac25ad323d63ed7a876d077b2c79d06
lordkenzo <lordkenzo@outlook.com>
2020-05-24 15:40:03 +0200

    add html element for json data

---
+++ public/js/load-persons.js
@@ -0,0 +1,12 @@
+export async function loadPersons() {
+  const data = await fetch('/.netlify/functions/get-persons')
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+
+  const persons = document.querySelector('persons');
+
+  const pre = document.createElement('pre');
+  pre.innerText = JSON.stringify(data, null, 2);
+
+  persons.appendChild(pre);
+}
